97f37cd7844c3a59a5ced1f17b1f3b7bcfea6a36
Elizondo Vicencio Victor     (LATAM) <3471866@cl.lan.com>
2020-03-03 07:21:56 -0300

    server con index 2

--- Treasuregram/Treasuregram/urls.py
+++ Treasuregram/Treasuregram/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+from main_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    #localhost/index
+    url(r'^index/', 
+    views.index),
+    ]
